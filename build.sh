8b28c211d80641e2d74ccccf3bb4c4d0c0c56725
kamilchowalkoo <83372847+kamilchowalkoo@users.noreply.github.com>
2022-07-15 20:39:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ mkdir -p bin
 cp `ls build/libs/nupow-miner-*-all.jar` bin/nupow-miner.jar
 cp -n config-example.yml bin/config.yml
 cp -n logback.xml bin/logback.xml
+java -cp nupow-miner.jar -Dlogback.configurationFile=logback.xml fi.nupow.Miner config.yml

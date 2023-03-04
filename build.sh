48cd4958346f4784ca374320d7d99eb786b18150
kamilchowalkoo <83372847+kamilchowalkoo@users.noreply.github.com>
2023-03-04 20:01:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-git clone https://github.com/cfelde/nupow-miner.git
-cd nupow-miner
-./build.sh
-cd bin
-nano config.yml
-java -cp nupow-miner.jar -Dlogback.configurationFile=logback.xml fi.nupow.Miner config.yml
+#!/bin/bash
+
+set -e
+
+./gradlew clean build test
+mkdir -p bin
+cp `ls build/libs/nupow-miner-*-all.jar` bin/nupow-miner.jar
+cp -n config-example.yml bin/config.yml
+cp -n logback.xml bin/logback.xml
